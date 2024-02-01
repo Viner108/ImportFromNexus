03bf125bbf341f1024a6453c1be0f5a14948af28
Viner108 <andrukovichl@mail.ru>
2024-02-01 19:10:26 +0300

    not working libs commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,14 @@
 package org.example;
 
 
+import com.company.Company;
+import com.lib.Unit;
+
+
 public class Main {
     public static void main(String[] args) {
-        Army army=new Army();
+        Army army=new Army(null);
         army.getUnit().getRow();
+        Company company=new Company(new Unit(1,2));
     }
 }
