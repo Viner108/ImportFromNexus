d1b2356de26dee799c7f97a72e0f4e5e98f4c6ec
Viner108 <andrukovichl@mail.ru>
2024-02-01 17:30:18 +0300

    doubleLib commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,9 @@
 package org.example;
 
-import com.lib.Unit;
 
 public class Main {
     public static void main(String[] args) {
-        Unit unit=new Unit();
+        Army army=new Army();
+        army.getUnit().getRow();
     }
 }
